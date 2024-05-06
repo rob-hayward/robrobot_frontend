3da69ec8ece8b157e7d1c55e20c34eb53de980e0
rob-hayward <hayward.m.rob@gmail.com>
2024-05-06 23:20:31 +0100

    home page intro and image grid complete

--- src/ProjectGrid.js
+++ src/ProjectGrid.js
@@ -4,23 +4,25 @@ import { Link } from 'react-router-dom';
 
 const ProjectGrid = ({ projects }) => {
   return (
-    <div className="project-grid">
-      {projects.map((project) => (
-        <div key={project.id} className="project-item">
-          <Link to={`/projects/${project.id}`}>
-            <div className="project-image">
-              <img
-                src={require(`./assets/images/${project.title.toLowerCase()}/${project.images[0].image.split('/').pop()}`)}
-                alt={project.title}
-              />
-              <div className="project-overlay">
-                <div className="project-title">{project.title}</div>
-                <div className="project-description">{project.synopsis}</div>
+    <div className="project-grid-container">
+      <div className="project-grid">
+        {projects.map((project) => (
+          <div key={project.id} className="project-item">
+            <Link to={`/projects/${project.id}`}>
+              <div className="project-image">
+                <img
+                  src={require(`./assets/images/${project.title.toLowerCase()}/${project.images[0].image.split('/').pop()}`)}
+                  alt={project.title}
+                />
+                <div className="project-overlay">
+                  <div className="project-title">{project.title}</div>
+                  <div className="project-description">{project.synopsis}</div>
+                </div>
               </div>
-            </div>
-          </Link>
-        </div>
-      ))}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
