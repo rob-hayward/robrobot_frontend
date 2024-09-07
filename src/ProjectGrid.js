e7f9ff64f1992d64c0d8dc24ca94d4e90d2d1de9
rob-hayward <hayward.m.rob@gmail.com>
2024-09-07 20:10:55 +0100

    Attempting to display new project images correctly when deployed

--- src/ProjectGrid.js
+++ src/ProjectGrid.js
@@ -1,11 +1,25 @@
-// src/ProjectGrid.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import techIconPaths from './techIcons';
+import config from './config';
 import './ProjectGrid.css';
 
 const ProjectGrid = ({ projects }) => {
+  const getImageSrc = (imagePath) => {
+    if (imagePath.startsWith('http') || imagePath.startsWith('/media/')) {
+      // This is a media file from the backend
+      return imagePath.startsWith('http') ? imagePath : `${config.API_BASE_URL}${imagePath}`;
+    } else {
+      // This is a static file from the frontend
+      try {
+        return require(`./assets/images/${imagePath}`);
+      } catch {
+        console.error(`Failed to load image: ${imagePath}`);
+        return ''; // or a placeholder image URL
+      }
+    }
+  };
+
   return (
     <div className="project-grid-container">
       <div className="project-grid-header">
@@ -13,28 +27,28 @@ const ProjectGrid = ({ projects }) => {
       </div>
       <div className="project-grid">
         {projects.map((project) => (
-            <div className="project-item">
-                <Link to={`/projects/${project.id}`}>
-                    <div className="project-image">
-                        <img
-                            src={require(`./assets/images/${project.title.toLowerCase()}/${project.images[0].image.split('/').pop()}`)}
-                            alt={project.title}
-                        />
-                        <div className="project-overlay">
-                            <div className="project-title">{project.title}</div>
-                            <div className="project-description">{project.synopsis}</div>
-                            <div className="technology-icons">
-                                {project.technologies_used.map(tech => (
-                                    <img key={tech.name}
-                                         src={require(`./assets/images/tech_icons/${techIconPaths[tech.name]}`)}
-                                         alt={tech.name}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
+          <div key={project.id} className="project-item">
+            <Link to={`/projects/${project.id}`}>
+              <div className="project-image">
+                <img
+                  src={getImageSrc(project.images[0].image)}
+                  alt={project.title}
+                />
+                <div className="project-overlay">
+                  <div className="project-title">{project.title}</div>
+                  <div className="project-description">{project.synopsis}</div>
+                  <div className="technology-icons">
+                    {project.technologies_used.map(tech => (
+                      <img key={tech.name}
+                           src={getImageSrc(tech.icon)}
+                           alt={tech.name}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
         ))}
       </div>
     </div>
